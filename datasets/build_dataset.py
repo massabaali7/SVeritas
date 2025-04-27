0b2afd479496f8e9e4e34271d7adce3c8718eb5e
Shravanth Srinivas <165218518+notshravanth@users.noreply.github.com>
2025-04-27 17:01:18 -0400

    V1

--- datasets/build_dataset.py
+++ datasets/build_dataset.py
@@ -2,7 +2,17 @@ from .libirspeech import LibriSpeech
 
 def build_dataset(dataset_name, config, device):
     if dataset_name == 'LibriSpeech':
-        dataset = LibriSpeech("librispeech_asr", split = 'test', partition = "clean", cache_dir=config['data_cache_dir'])
+        dataset = LibriSpeech(
+            "librispeech_asr", 
+            split = 'test', 
+            partition = "clean", 
+            cache_dir=config['data_cache_dir'])
+    elif dataset_name == 'TEDLIUM':
+        dataset = TEDLIUM(
+            "LIUM/tedlium",       
+            split='test',          
+            partition="release3", 
+            cache_dir=config['data_cache_dir']
     else: 
         raise NotImplementedError
 
