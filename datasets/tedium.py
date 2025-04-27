b94aee67f6e24bc062c3d7e76b196ce5436248f8
Shravanth Srinivas <165218518+notshravanth@users.noreply.github.com>
2025-04-27 16:59:29 -0400

    V1

--- datasets/tedium.py
+++ datasets/tedium.py
@@ -1,1 +1,24 @@
+from datasets import load_dataset
+import torch
 
+class TEDLIUM(torch.nn.Module):
+    def __init__(
+        self,
+        dataset_name: str = "LIUM/tedlium",
+        split: str = "test",
+        partition: str = "release3",
+        cache_dir: str = None
+    ):
+        super(TEDLIUM, self).__init__()
+        self.dataset_name = dataset_name
+        self.partition    = partition
+        self.split        = split
+        self.cache_dir    = cache_dir
+
+    def forward(self, audio):
+        return load_dataset(
+            self.dataset_name,
+            self.partition,
+            split=self.split,
+            cache_dir=self.cache_dir
+        )
