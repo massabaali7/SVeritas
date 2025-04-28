bbf79c50209f28dd68fd40dcc2b426be1ff401a8
Shravanth Srinivas <165218518+notshravanth@users.noreply.github.com>
2025-04-28 10:11:29 -0400

    V1

--- datasets/build_dataset.py
+++ datasets/build_dataset.py
@@ -1,4 +1,6 @@
 from .libirspeech import LibriSpeech
+from .tedlium    import TEDLIUM
+from .ami        import AMI
 
 def build_dataset(dataset_name, config, device):
     if dataset_name == 'LibriSpeech':
@@ -12,6 +14,12 @@ def build_dataset(dataset_name, config, device):
             "LIUM/tedlium",       
             split='test',          
             partition="release3", 
+            cache_dir=config['data_cache_dir']
+    elif dataset_name == 'AMI':
+        dataset = AMI(
+            "edinburghcstr/ami",  
+            split='test',        
+            partition="ihm",   #ihm or sdm     
             cache_dir=config['data_cache_dir']
     else: 
         raise NotImplementedError
