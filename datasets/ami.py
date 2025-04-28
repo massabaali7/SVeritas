e38e320ab3ff19db0c2df41d210d9ea1430cad07
Shravanth Srinivas <165218518+notshravanth@users.noreply.github.com>
2025-04-28 10:08:10 -0400

    V1

---
+++ datasets/ami.py
@@ -0,0 +1,24 @@
+from datasets import load_dataset
+import torch
+
+class AMI(torch.nn.Module):
+    def __init__(
+        self,
+        dataset_name: str = "edinburghcstr/ami",
+        split: str = "test",
+        partition: str = "ihm",
+        cache_dir: str = None
+    ):
+        super(AMI, self).__init__()
+        self.dataset_name = dataset_name
+        self.partition    = partition
+        self.split        = split
+        self.cache_dir    = cache_dir
+
+    def forward(self, audio):
+        return load_dataset(
+            self.dataset_name,
+            self.partition,
+            split=self.split,
+            cache_dir=self.cache_dir
+        )
