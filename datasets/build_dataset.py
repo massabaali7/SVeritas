af516852679ead29aa4c73b218d1888241d54ba1
Shravanth Srinivas <165218518+notshravanth@users.noreply.github.com>
2025-04-30 11:00:38 -0400

    Update build_data.py

--- datasets/build_dataset.py
+++ datasets/build_dataset.py
@@ -1,6 +1,7 @@
 from .libirspeech import LibriSpeech
 from .tedlium    import TEDLIUM
-from .ami        import Ami
+from .ami        import AMI
+from .voxpopuli import VoxPopuli
 
 def build_dataset(dataset_name, config, device):
     if dataset_name == 'LibriSpeech':
@@ -14,13 +15,18 @@ def build_dataset(dataset_name, config, device):
             "LIUM/tedlium",       
             split='test',          
             partition="release3", 
-            cache_dir=config['data_cache_dir']
+            cache_dir=config['data_cache_dir'])
     elif dataset_name == 'AMI':
-        dataset = Ami(
+        dataset = AMI(
             "edinburghcstr/ami",  
             split='test',        
             partition="ihm",   #ihm or sdm     
-            cache_dir=config['data_cache_dir']
+            cache_dir=config['data_cache_dir'])
+    elif dataset_name == 'VoxPopuli':
+        dataset = VoxPopuli(
+            dataset_name="macabdul9/voxpopuli_en_accented_SpeakerVerification", 
+            split='test', 
+            cache_dir=config['data_cache_dir'])
     else: 
         raise NotImplementedError
 
