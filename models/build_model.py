2cced7698462fde3aedfa296e1fef955dc6a0e5c
massabaali7 <100831623+massabaali7@users.noreply.github.com>
2025-05-08 16:56:51 -0400

    Added mfa_conformer

--- models/build_model.py
+++ models/build_model.py
@@ -4,7 +4,7 @@ from .redimnet import Redimnet
 from .titanet import Titanet
 from .resnet34LM import resnet34_LM
 from .wavLMBase import wavLMBase
-
+from .mfa_conformer_lightning import MFA_Conformer
 def build_model(model, config):
     if model == 'ECAPA2':
         model = ECAPA2(model_location = config['device'], cache_dir=config['model_dir'])
@@ -20,7 +20,11 @@ def build_model(model, config):
         model = wavLMBase(model_name = "microsoft/wavlm-base-sv", model_location = config['device'])
     elif model == 'wavLMBasePlus':
         model = wavLMBase(model_name = "microsoft/wavlm-base-plus-sv", model_location = config['device'])
-    
+    elif model == 'MFA_Conformer':
+        model = MFA_Conformer(model_location = config['device'], ckpt = "/storage1/avis_spk_id/baseline_framework/ckpt/epoch=17_cosine_eer=0.86.ckpt", lr=0.001 )
+    elif model == 'MFA_Conformer_CAARMA':
+        model = MFA_Conformer(model_location = config['device'], ckpt = "/storage1/avis_spk_id/baseline_framework/ckpt/epoch=97_cosine_eer=0.83.ckpt", lr=0.0045 )
+
     else: 
         raise NotImplementedError
 
